2d4e1e0fbb4a38ad10111e8c6e58fdfcf5b4fe1e
Rey Lim <Reylim15@gmail.com>
2019-07-03 01:50:30 -0400

    Added easy-table package reccommended by Jorge

--- Js/bamazonCustomer.js
+++ Js/bamazonCustomer.js
@@ -1,2 +1,13 @@
+// Npm Packages
+
 var inquirer = require("inquirer");
+var Table = require('easy-table')
 var mysql = require("mysql");
+
+// Connection to mysl server
+var connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"Bamazon_db"
+});
